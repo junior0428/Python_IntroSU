6f2b47d18d635d12d08208941fe3e6e7690734f9
Junior <juniorantoniocalvomontanez@gmail.com>
2021-01-16 01:54:42 -0600

    Modifyling the code

--- Intro.py
+++ Intro.py
@@ -3,5 +3,10 @@
 
 2+3 #add twon numbers
 10 % 3 #Modulus 
+# "Print" is another funtion in python lenguages
+
+print("navin´s laptop")# can add inside print ¨¨
+
+10*"Navin" #printing ten time "Navin"
 
 
