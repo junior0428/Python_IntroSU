5fec1170c1c6406ce819b5b3e71bbd7c55918f86
Junior <juniorantoniocalvomontanez@gmail.com>
2021-01-16 02:22:46 -0600

    Modilying the code with more word and examples

--- Intro.py
+++ Intro.py
@@ -9,4 +9,30 @@ print("navin´s laptop")# can add inside print ¨¨
 
 10*"Navin" #printing ten time "Navin"
 
+#.......Objects, Expresions, and Numerical types
+# 1° int 
+# 2°foat
+# 3°bool ....>True and False
+# 4° None 
+
+type(3) # "int" to know the type of object
+type(3.4) #float
+
+#operators "+" "-" "*" "//"(interger division) "/" (float) "%"(mod) "**"() 
+2**6 #The raised to the power 2^6
+
+#"=="..equal, "!="...not equal, 
+# ">"..greater, "<" ..at least and >= ..at most
+
+
+#..........Variables and assignment ...................
+pi=3
+pi+4
+radius=11 #you must defined the variable to run the next
+radius+2
+#remember that variables is just a name and the assignmet associate the name with object
+
+
+
+
 
