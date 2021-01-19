df0eef0bd6fc452b7103e1210687bdf24b5cc116
Junior <juniorantoniocalvomontanez@gmail.com>
2021-01-19 08:37:54 -0600

    practice with examples

---
+++ funtion.py
@@ -0,0 +1,24 @@
+
+
+def resta():
+    print("Hola como estan esto es un ejemplo de funciones")
+
+resta()
+
+def numero():
+    nu1=40
+    nu2=50
+    print(nu1+nu2)
+
+numero()
+
+def numero1(a,b):
+    print(a*b)
+
+numero1(40052245524524,50045244524452455)
+
+def sume(x,y):
+    result=x+y
+    return result
+
+print(sume(2,3))
